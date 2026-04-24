7f8ce88d180daf836e1f726c47dacc646c575594
oluwasemilore-adesua <70629635+oluwasemilore-adesua@users.noreply.github.com>
2026-04-24 17:11:23 +0100

    routes

--- src/routes/carts/cart.routes.js
+++ src/routes/carts/cart.routes.js
@@ -13,6 +13,6 @@ const auth = require("../../middlewares/auth.middleware");
 router.get("/", auth, getCart);
 router.post("/", auth, addToCart);
 router.put("/items/:productId", auth, updateCartItem);
-router.delete("/clear", auth, clearCart);
+router.delete("/", auth, clearCart);
 
 module.exports = router;
